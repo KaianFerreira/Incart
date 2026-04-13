01158ebef002dbc6b2d28afa0fb1990b475084f8
Kaian Santos <Kaianfsantos@gmail.com>
2026-04-13 13:45:00 -0300

    feat(store): validated cart state with Zustand

---
+++ store/useCartStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand"
+
+export type CartItem = {
+  id: string
+  name: string
+  /** Unit price in BRL */
+  price: number
+}
+
+type CartState = {
+  items: CartItem[]
+  addItem: (item: CartItem) => void
+  removeItem: (id: string) => void
+  clearCart: () => void
+}
+
+export const useCartStore = create<CartState>((set) => ({
+  items: [],
+  addItem: (item) =>
+    set((state) => ({
+      items: [...state.items, item],
+    })),
+  removeItem: (id) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== id),
+    })),
+  clearCart: () => set({ items: [] }),
+}))
